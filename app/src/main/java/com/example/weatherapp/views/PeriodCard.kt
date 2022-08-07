f056ce4128ffb57a9e088f8a1617c5588918b330
XavierLQ <96361187+XavierLQ@users.noreply.github.com>
2022-08-06 21:54:29 -0600

    Created the period card which fills the individual data for each period.

--- app/src/main/java/com/example/weatherapp/views/PeriodCard.kt
+++ app/src/main/java/com/example/weatherapp/views/PeriodCard.kt
@@ -1,20 +1,23 @@
 package com.example.weatherapp.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.weatherapp.model.Period
 
 
 @Composable
 fun PeriodCard(period: Period) {
 
-    val name: String = period.name
-    val temp: String = period.temperature.toString()
-    val tempUnit: String = period.temperatureUnit
-    val windSpeed: String = period.windSpeed
-    val windDir: String = period.windDirection
-    val shortForecast: String = period.shortForecast
-    val longForecast: String = period.longForecast
-
-
-
+    Column(modifier = Modifier.padding(all = 10.dp)) {
+        Text(text = "Name: ${period.name}")
+        Text(text = "Temperature: ${period.temperature} ${period.temperatureUnit}")
+        Text(text = "Wind Speed: ${period.windSpeed}")
+        Text(text = "Wind Direction: ${period.windDirection}")
+        Text(text = "Short Forecast: ${period.shortForecast}")
+        Text(text = "Long Forecast: ${period.longForecast}")
+    }
 }
