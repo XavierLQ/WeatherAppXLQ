162f17c1d12ad571d2251e9a3b7855a6501c4c7f
XavierLQ <96361187+XavierLQ@users.noreply.github.com>
2022-08-06 19:07:04 -0600

    Add localData module which opens the local json file and parses it into POJO object

---
+++ app/src/main/java/com/example/weatherapp/dataSource/localData.kt
@@ -0,0 +1,35 @@
+package com.example.weatherapp.dataSource
+
+import android.content.Context
+import android.util.JsonReader
+import com.example.weatherapp.R
+import com.example.weatherapp.model.Forecast
+import com.example.weatherapp.model.Period
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import java.io.InputStream
+import java.io.InputStreamReader
+
+class localData {
+
+    fun jsonToPojoConverter(json: JsonReader): Forecast? {
+        val moshi: Moshi = Moshi.Builder().build()
+        val adapter: JsonAdapter<Forecast> = moshi.adapter(Forecast::class.java)
+        return adapter.fromJson(json.toString())
+    }
+
+    fun getLocalJsonData(path: String, context: Context):JsonReader{
+        val input: InputStream = context.resources.openRawResource(R.raw.weather)
+        val json = JsonReader(InputStreamReader(input))
+        return json
+    }
+
+    fun getPeriodData(forecast: Forecast): List<Period>{
+        val periodList: List<Period> = forecast.periods
+        return periodList
+    }
+
+    companion object{
+        const val weatherJsonPath = "weather.json"
+    }
+}
